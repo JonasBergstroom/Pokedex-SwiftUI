53604eb2eb25551f6c96a55a5bf19a18c53cbfd2
Jonas Bergström <jontrian97@gmail.com>
2023-11-30 18:43:37 +0100

    Add searching and filtering

--- Pokedex-SwiftUI/ViewModels/ViewModel.swift
+++ Pokedex-SwiftUI/ViewModels/ViewModel.swift
@@ -8,4 +8,16 @@
 import Foundation
 import SwiftUI
 
-
+final class ViewModel: ObservableObject {
+    private let pokemonManager = PokemonManager()
+    
+    @Published var pokemonList = [Pokemon]()
+    @Published var infoPokemon: InfoPokemon?
+    @Published var searchText = ""
+    
+    var filteredPokemon: [Pokemon] {
+        return searchText == "" ? pokemonList : pokemonList.filter {
+            $0.name.contains(searchText.lowercased())
+        }
+    }
+}
